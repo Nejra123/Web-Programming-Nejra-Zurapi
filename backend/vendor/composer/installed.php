fba04ddb9f52849ccce78b4254fc8c7da2ac479d
Nejra Zurapi <nejra.zurapi@stu.ibu.edu.ba>
2025-10-29 21:42:37 +0100

    additional files

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e325eec99daa3fc10061be30f3ca129ac12b13e2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e325eec99daa3fc10061be30f3ca129ac12b13e2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'mikecao/flight' => array(
+            'pretty_version' => 'v3.17.2',
+            'version' => '3.17.2.0',
+            'reference' => '095a46663f7090c549f5c3459f0dada893b3b5d9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mikecao/flight',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
