c70167671ee643c8810d4958f01f3b4728fe0015
Nejra Zurapi <nejra.zurapi@stu.ibu.edu.ba>
2025-12-10 16:34:47 +0100

    removing function from html

---
+++ frontend/services/contact-service.js
@@ -0,0 +1,74 @@
+let MessageService = {
+    sendMessage: function(messageData, callback) {
+        const user = UserService.getCurrentUser();
+        
+        if (!user) {
+            toastr.error("Please login to send a message");
+            window.location.hash = "#profile";
+            return;
+        }
+    
+        
+        const customerId = user.ID || user.id;
+        const username = user.username || user.email || user.name || 'Guest';
+        
+        if (!customerId) {
+            toastr.error("Could not determine user ID");
+            return;
+        }
+        
+        const data = {
+            username: username,
+            content: messageData.content,
+            customer_id: customerId
+        };
+        
+
+        RestClient.post('messages', data, function(response) {
+            toastr.success("Message sent successfully!");
+            if (callback) callback(response);
+        }, function(jqXHR) {
+            console.error("Error sending message:", jqXHR);
+            const errorMsg = jqXHR.responseJSON?.error || 
+                           jqXHR.responseJSON?.message || 
+                           'Failed to send message';
+            toastr.error(errorMsg);
+        });
+    }
+};
+
+let ContactService = {
+    init: function() {
+        this.setupContactForm();
+    },
+
+    setupContactForm: function() {
+        const self = this;
+        
+        $('#contact-form').off('submit').on('submit', function(e) {
+            e.preventDefault();
+            
+            if (!UserService.isLoggedIn()) {
+                toastr.error("Please login to send a message");
+                window.location.hash = "#profile";
+                return;
+            }
+            
+            const formData = Object.fromEntries(new FormData(this).entries());
+            
+            const submitBtn = $(this).find('button[type="submit"]');
+            const originalText = submitBtn.html();
+            submitBtn.prop('disabled', true).html('<i class="fa fa-spinner fa-spin me-2"></i> Sending...');
+            
+            MessageService.sendMessage(formData, function(response) {
+                console.log("Message sent successfully");
+                $('#contact-form')[0].reset();
+                submitBtn.prop('disabled', false).html(originalText);
+            });
+        });
+    },
+
+    refresh: function() {
+        this.setupContactForm();
+    }
+};
